bdae165cddeaa0da47d726ec339f8c4cef827ccb
Pierre Oblin <pierre.oblin@orange.com>
2016-07-09 11:52:19 +0200

    use install.sh provided by xen tools rather than just install deb package

--- stemcell_builder/stages/system_ubuntu_xen_tools/apply.sh
+++ stemcell_builder/stages/system_ubuntu_xen_tools/apply.sh
@@ -7,14 +7,10 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
-
-
 #download xenserver tools iso distrib to extract tools
-
 
 rm -rf ${work}/xenserver
 mkdir ${work}/xenserver
-#wget http://downloadns.citrix.com.edgesuite.net/9286/XenServer-6.2.0-SP1-XS62ESP1003-xs-tools-6.2.0-5.iso?_ga=1.263249609.455660746.1421837492 -O ${work}/xenserver/XenServer-6.2.0-SP1-XS62ESP1003-xs-tools-6.2.0-5.iso
 wget http://boot.rackspace.com/files/xentools/xs-tools-6.5.0-20200.iso -O ${work}/xenserver/xs-tools-6.5.0-20200.iso  
 
 
@@ -25,14 +21,12 @@ echo "Mounting xenserver iso from at $iso_mount_path"
 mount -o loop -t iso9660 ${work}/xenserver/xs-tools-6.5.0-20200.iso $iso_mount_path
 add_on_exit "umount $iso_mount_path"
 
-#mirror="file://$iso_mount_path"
-
 
 #extract and copy tools
 cp $iso_mount_path/Linux/xe-guest-utilities_*.deb $chroot/tmp
+cp $iso_mount_path/Linux/install.sh $chroot/tmp
+
 
 #install tools in chroot
-run_in_chroot $chroot "sudo dpkg -i /tmp/xe-guest-utilities_6.2.0-1137_amd64.deb"
-
-
-
+#run_in_chroot $chroot "sudo dpkg -i /tmp/xe-guest-utilities_6.5.0-1427_amd64.deb"
+run_in_chroot $chroot "sudo dpkg -i /tmp/install.sh"
