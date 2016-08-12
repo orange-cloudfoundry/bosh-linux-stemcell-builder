199593efb24e0b1c83a5d057943b240e0c9470ca
poblin-orange <pierre.oblin@orange.com>
2016-08-12 13:31:53 +0200

    fix issue https://github.com/cloudfoundry-community/bosh-cloudstack-cpi-release/issues/22

--- stemcell_builder/stages/prepare_vhdx_image_stemcell/apply.sh
+++ stemcell_builder/stages/prepare_vhdx_image_stemcell/apply.sh
@@ -1,34 +1,20 @@
 #!/usr/bin/env bash
 #
-
-
 set -e
 
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
 rm -f $work/root.vhd
-rm -f $work/0.vhd 
 
 
 # Convert raw to dynamic vhd
 qemu-img convert -O vpc -o subformat=dynamic $work/${stemcell_image_name} $work/root.vhd
 
-
-# targeting xenserver vhd acceptable format. use img => qcow2 => raw
-#qemu-img convert -c -O qcow2 $work/${stemcell_image_name} $work/0.qcow2
-#qemu-img convert -O raw $work/0.qcow2 $work/0.raw
-
-#add faketime
-sudo apt-get install -y faketime
-
-
-#vhd-utils does only raw => fixed, or fixed => dynamic. chaining 2 conversions
-#vhd-util convert -i $work/0.raw -s 0 -t 1  -o $work/0.vhd 
-#faketime '2010-01-01' vhd-util convert -i $work/0.vhd -s 1 -t 2  -o $work/root.vhd 
-
 #Verification: 
 vhd-util check -n $work/root.vhd 
+vhd-util read -p -n $work/root.vhd
+ls -lh $work/root.vhd
 
 pushd $work
 bzip2 -c root.vhd > stemcell/image
