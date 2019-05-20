35856d76de31fe5a5637a79eefef32562f1faded
Xavier MARCELET <xavier.marcelet@orange.com>
2019-05-20 12:28:41 +0200

    bump xen tools

--- stemcell_builder/stages/system_ubuntu_xen_tools/apply.sh
+++ stemcell_builder/stages/system_ubuntu_xen_tools/apply.sh
@@ -8,7 +8,7 @@ source $base_dir/lib/prelude_apply.bash
 
 # uncompress xen-tools into chroot's tmp
 mkdir -p $chroot/tmp/xen-tools/
-tar xvzf $(dirname $0)/assets/xgu-7.3.0-1.tar.gz -C $chroot/tmp/xen-tools/
+tar xvzf $(dirname $0)/assets/xgu-7.10.0-1.tar.gz -C $chroot/tmp/xen-tools/
 
 # run xen-tools install script
 run_in_chroot $chroot "sudo bash /tmp/xen-tools/install.sh"
