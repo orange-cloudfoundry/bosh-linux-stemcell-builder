0a9a7acb923be7537e569db2a7b39c6cc3021dd5
Xavier MARCELET <xavier.marcelet@orange.com>
2019-06-23 15:32:33 +0200

    add missing http proxy handling in system_vhd_utils_tools

--- stemcell_builder/stages/system_vhd_utils_tools/apply.sh
+++ stemcell_builder/stages/system_vhd_utils_tools/apply.sh
@@ -5,15 +5,21 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
-sudo apt-get install -y uuid-dev
+opt=""
+if [ ! -z "${http_proxy}" ]; then
+    opt="${opt} -o Acquire::http::Proxy=http://${http_proxy}"
+fi
+
+if [ ! -z "${https_proxy}" ]; then
+    opt="${opt} -o Acquire::https::Proxy=https://${https_proxy}"
+fi
+sudo apt-get install -y uuid-dev ${opt}
+
 cd /tmp
-rm -rf vhd-util-convert 
+rm -rf vhd-util-convert
 #git clone https://github.com/rubiojr/vhd-util-convert
 git clone https://github.com/rubiojr/vhd-util-convert
 cd vhd-util-convert
 make
 #install vhd-util in the vagrant stemcell builder host
 sudo cp vhd-util /usr/local/bin/
-
-
-
