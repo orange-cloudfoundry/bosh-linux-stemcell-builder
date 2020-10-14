ca5bb6903fe50e66d12a673dccf7db89e550f8cc
Xavier MARCELET <xavier.marcelet@orange.com>
2020-10-14 13:46:08 +0200

    fix cloudstack install step

--- stemcell_builder/stages/bosh_cloudstack_ubuntu_vr_metadata/assets/install.sh
+++ stemcell_builder/stages/bosh_cloudstack_ubuntu_vr_metadata/assets/install.sh
@@ -11,13 +11,14 @@ cd ${dir}
 
 curl -L https://github.com/${repo}/releases/download/v${version}/${file} --output ${file}
 tar xzf ${file}
+ls -la ${name}
 
 mkdir -p /usr/share/mdproxy4cs/
 
-cp ${name}/mdproxy4cs         /usr/bin/
-cp ${name}/pre-start.sh       /usr/share/mdproxy4cs/pre-start.sh
-cp ${name}/mdproxy4cs.service /usr/share/mdproxy4cs/mdproxy4cs.service
-cp ${name}/default            /etc/default/mdproxy4cs
+cp ${name}/mdproxy4cs                /usr/bin/
+cp ${name}/assets/pre-start.sh       /usr/share/mdproxy4cs/pre-start.sh
+cp ${name}/assets/mdproxy4cs.service /usr/share/mdproxy4cs/mdproxy4cs.service
+cp ${name}/assets/default            /etc/default/mdproxy4cs
 
 systemctl enable /usr/share/mdproxy4cs/mdproxy4cs.service
 
